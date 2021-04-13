c2ce1051c03c0d515ca15ac4a15586ecb4da2ddd
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-04-13 22:46:49 +0200

    update the correlated_disturbances function

--- disturbancesGnr.py
+++ disturbancesGnr.py
@@ -1,7 +1,8 @@
 from numpy.random import multivariate_normal
 import numpy as np
 from numpy.random import seed
-
+from numpy.linalg import matrix_power
+from scipy.linalg import fractional_matrix_power
 
 def normaldisturbances(n,k,sigma):
     """
@@ -25,3 +26,11 @@ def normaldisturbances(n,k,sigma):
     cov = np.eye(n)*sigma 
     mu = np.zeros(n)
     return multivariate_normal(mu,cov,k)
+
+
+
+def correlated_disturbances(delta,alpha,tau,n,L,k):
+    cov_matrix = delta*tau**(2*alpha)*fractional_matrix_power(L+tau**(2*alpha)*np.eye(n),-alpha)
+    seed(100)
+    mu = np.zeros
+    return multivariate_normal(mu,cov_matrix,k)
