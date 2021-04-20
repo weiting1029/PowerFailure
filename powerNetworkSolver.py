f634ba8afdc5dfa7d86324ea63a8b79e7477146b
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-04-20 21:51:49 +0200

    minor fix

--- powerNetworkSolver.py
+++ powerNetworkSolver.py
@@ -48,18 +48,18 @@ class PowerNetworkSolver(object):
                 np.multiply(sinmatrix, self.A), axis=0))
         return sol_domega
 
-    def Simulation(self, KK, check_times, sigma, thres, t, nn, dtb_gnr):
+    def Simulation(self, KK, check_times, sigma, thres, t, nn, disturbances):
         starttime = time.time()
         n = self.ngnr
         dt = np.linspace(0, t, nn + 1)
         vcheck_omega = np.zeros(n)
-        vcheck_theta = np.zeros(n)
+        vcheck_domega = np.zeros(n)
         vcheck_any = np.zeros(n)
         mcheck_omega = np.zeros((n, n))
-        mcheck_theta = np.zeros((n, n))
+        mcheck_domega = np.zeros((n, n))
         mcheck_any = np.zeros((n, n))
 
-        disturbances = dtb_gnr(n, KK, sigma)
+        # disturbances = dtb_gnr(n, KK, sigma)
 
         for k in range(KK):
             sol0 = np.pad(disturbances[k], (n, 0), 'constant', constant_values=(0, 0))
@@ -67,24 +67,25 @@ class PowerNetworkSolver(object):
             sol_domega = self.getDotOmega(vec_sol[:, :n], vec_sol[:, n:], nn)  # get numerical tracks for omega dot
             omega_domega = np.concatenate((vec_sol[:, n:], sol_domega), axis=1)  # create a vector of [omega,domega]
             all_check = globalcheck(omega_domega, check_times, thres, n, nn)
-            vcheck_theta += all_check[0]
-            vcheck_omega += all_check[1]
+            vcheck_omega += all_check[0]
+            vcheck_domega += all_check[1]
             vcheck_any += all_check[2]
 
-            mcheck_theta += np.outer(all_check[0], all_check[0])
-            mcheck_omega += np.outer(all_check[1], all_check[1])
+            mcheck_omega += np.outer(all_check[0], all_check[0])
+            mcheck_domega += np.outer(all_check[1], all_check[1])
             mcheck_any += np.outer(all_check[2], all_check[2])
 
         vcheck_omega = vcheck_omega / KK
-        vcheck_theta = vcheck_theta / KK
+        vcheck_domega = vcheck_domega / KK
         vcheck_any = vcheck_any / KK
-        mcheck_theta = mcheck_theta / KK
         mcheck_omega = mcheck_omega / KK
+        mcheck_domega = mcheck_domega / KK
         mcheck_any = mcheck_any / KK
 
         stoptime = time.time()
         totaltime = stoptime - starttime
-        return {'vcheck_omega': vcheck_omega, 'vcheck_theta': vcheck_theta, 'vcheck_any': vcheck_any,
-                'mcheck_theta': mcheck_theta, 'mcheck_any': mcheck_any, 'totaltime': "{:2.2}sec".format(totaltime)}
+        return {'vcheck_omega': vcheck_omega, 'vcheck_domega': vcheck_domega, 'vcheck_any': vcheck_any,
+                'mcheck_omega': mcheck_omega, 'mcheck_domega': mcheck_domega, 'mcheck_any': mcheck_any,
+                'totaltime': "{:2.2}sec".format(totaltime)}
 
 #
