4b5aa1b13d8a2fbebd230ae6bae77f946cc39518
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-04-21 00:27:00 +0200

    something random

--- rate_table.py
+++ rate_table.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from powerNetworkSolver import PowerNetworkSolver
-from powerNetwork import networkTransform, getGenerators, getBuses, getLines,getNumBuses, getNumLines
+from powerNetwork import networkTransform, getGenerators, getBuses, getLines, getNumBuses, getNumLines
 from powerNetwork import getUndGraph, kron_reduction, edge_removing
 from disturbancesGnr import normaldisturbances
 from violationChecking import globalcheck
-import seaborn as sns; sns.set_theme()
-#importing the function for multi-edge removal
+import seaborn as sns;
+
+sns.set_theme()
+# importing the function for multi-edge removal
 from powerNetwork import multi_edge_removing
 import matplotlib.pyplot as plt
 
@@ -14,7 +16,7 @@ import copy
 from numpy.linalg import inv
 import networkx as nx
 from numpy.random import multivariate_normal
-from disturbancesGnr import normaldisturbances,correlated_disturbances
+from disturbancesGnr import normaldisturbances, correlated_disturbances
 from scipy.integrate import odeint
 from violationChecking import globalcheck
 import time
@@ -31,81 +33,114 @@ network39, subnetwork39 = networkTransform(case39())
 df_lines39 = getLines(subnetwork39)
 df_buses39 = getBuses(subnetwork39)
 
-n39, ngnr39, unG39 = getUndGraph(df_buses39,df_lines39, network39)
-A39, redL39, redA39  = kron_reduction(n39, ngnr39, unG39)
-node_list =  np.arange(ngnr39)+1
+n39, ngnr39, unG39 = getUndGraph(df_buses39, df_lines39, network39)
+A39, redL39, redA39 = kron_reduction(n39, ngnr39, unG39)
+node_list = np.arange(ngnr39) + 1
 
 K = 1
 M = np.array([0.2228, 0.1607, 0.1899, 0.1517, 0.1379, 0.1846, 0.1401, 0.1289, 0.183, 2.6526])
-D = np.array([0.0332,0.076,0.0862,0.0838,0.0674,0.0862,0.0743,0.0716,0.1101,0.1333])
+D = np.array([0.0332, 0.076, 0.0862, 0.0838, 0.0674, 0.0862, 0.0743, 0.0716, 0.1101, 0.1333])
 # Ome = np.zeros(n)
 pi = math.pi
 t = 2
 nn = 100
-dt = np.linspace(0, t, nn+1)
+dt = np.linspace(0, t, nn + 1)
 
 sigma = 0.01
 theta0 = np.zeros(ngnr39)
 omega0 = np.zeros(ngnr39)
 OMEGA = np.zeros(ngnr39)
-model39 = PowerNetworkSolver(theta0,omega0,redA39, ngnr39, D,M,K,OMEGA)
+model39 = PowerNetworkSolver(theta0, omega0, redA39, ngnr39, D, M, K, OMEGA)
 
 from numpy.random import seed
 
-#generate numerical tracks
+# generate numerical tracks
 seed(100)
 disturbances = correlated_disturbances(delta=1, alpha=1.5, tau=1.5, n=ngnr39, L=redL39, k=1)
 # disturbances = normaldisturbances(n=ngnr39,k=1,sigma=sigma)
-sol0 = np.pad(disturbances[0], (ngnr39,0), 'constant', constant_values=(0,0))
-single_sol = model39.solkuramoto(sol0,dt)
-sol_domega = model39.getDotOmega(single_sol[:,:ngnr39],single_sol[:,ngnr39:],nn)
+sol0 = np.pad(disturbances[0], (ngnr39, 0), 'constant', constant_values=(0, 0))
+single_sol = model39.solkuramoto(sol0, dt)
+sol_domega = model39.getDotOmega(single_sol[:, :ngnr39], single_sol[:, ngnr39:], nn)
+
+# fig = plt.figure()
+# for i in range(ngnr39):
+#     theta = single_sol[:,i]
+#     plt.plot(dt, theta, "-", label="Theta"+ str(i+1))
+#
+# plt.xlabel("Time")
+# plt.ylabel("Theta")
+# plt.ylim(-0.2, 1)
+# plt.legend();
+# plt.title('Phase angles')
+# # plt.show()
+# # plt.interactive(True)
+# plt.savefig('theta_corcov.png')
+#
+#
+# fig = plt.figure()
+# for i in range(ngnr39):
+#     omega = single_sol[:,ngnr39+i]
+#     plt.plot(dt, omega, "-", label="Omega"+ str(i+1))
+#
+#
+# plt.xlabel("Time")
+#
+# plt.ylabel("Omega")
+# plt.ylim(0, 1)
+# plt.legend()
+# plt.title('Natural rotation frequencies')
+# plt.savefig('omega_corcov.png')
+#
+#
+#
+#
+# fig = plt.figure()
+# for i in range(ngnr39):
+#     domega = sol_domega[:,i]
+#     plt.plot(dt, domega, "-", label="Omega Dot"+ str(i+1))
+#
+# plt.xlabel("Time")
+# plt.ylabel("Omega Dot")
+# plt.ylim(-5, 5)
+# plt.legend();
+# plt.title('Change Rates of Natural rotation frequencies')
+# plt.savefig('domega_corcov.png')
+#
 
 
-fig = plt.figure()
-for i in range(ngnr39):
-    theta = single_sol[:,i]
-    plt.plot(dt, theta, "-", label="Theta"+ str(i+1))
+check_times = 100
+KK = 3000  # repetition times
+thres = np.array([0.2, 2])  # thres1 is for omega, thres2 is for omega_dot
+# iid_disturbances = normaldisturbances(n=ngnr39, k=KK, sigma=sigma)
+cor_disturbances = correlated_disturbances(delta=1, alpha=1.5, tau=1.5, n=ngnr39, L=redL39, k=KK)
+# rates39 = model39.Simulation(KK, check_times, sigma, thres, t, nn, cor_disturbances)
+# df39 = pd.DataFrame({'Node': node_list, 'RoCoF': rates39['vcheck_domega'],
+#                      'AFV': rates39['vcheck_omega'], 'AV': rates39['vcheck_any']})
+# # print(df39.to_latex(index=False))
+# df39.to_excel('tables\original_network_cor.xlsx')
 
-plt.xlabel("Time")
-plt.ylabel("Theta")
-plt.ylim(-0.2, 1)
-plt.legend();
-plt.title('Phase angles')
-# plt.show()
-# plt.interactive(True)
-plt.savefig('theta_corcov.png')
+G3905 = multi_edge_removing(unG39, ([4, 5], [15, 16], [4, 14]))
+A3905, redL3905, redA3905 = kron_reduction(n39, ngnr39, G3905)
+model3905 = PowerNetworkSolver(theta0, omega0, redA3905, ngnr39, D, M, K, OMEGA)
+rates3905 = model3905.Simulation(KK, check_times, sigma, thres, t, nn, cor_disturbances)
+df3905 = pd.DataFrame({'Node': node_list, 'RoCoF': rates3905['vcheck_domega'],
+                     'AFV': rates3905['vcheck_omega'], 'AV': rates3905['vcheck_any']})
 
+df3905.to_excel('tuple_cor.xlsx')
 
-fig = plt.figure()
-for i in range(ngnr39):
-    omega = single_sol[:,ngnr39+i]
-    plt.plot(dt, omega, "-", label="Omega"+ str(i+1))
-
-
-plt.xlabel("Time")
-
-plt.ylabel("Omega")
-plt.ylim(0, 1)
-plt.legend()
-plt.title('Natural rotation frequencies')
-plt.savefig('omega_corcov.png')
-
-
-
-
-fig = plt.figure()
-for i in range(ngnr39):
-    domega = sol_domega[:,i]
-    plt.plot(dt, domega, "-", label="Omega Dot"+ str(i+1))
-
-plt.xlabel("Time")
-plt.ylabel("Omega Dot")
-plt.ylim(-5, 5)
-plt.legend();
-plt.title('Change Rates of Natural rotation frequencies')
-plt.savefig('domega_corcov.png')
-
-
-
-
-
+# edge_list39 = list(unG39.edges)
+# num_edges39 = getNumLines(getLines(subnetwork39))
+# rate_list39 = np.zeros((num_edges39, 4))
+# # print()
+# j = 0
+# for i in edge_list39:
+#     temp_edge = np.array(i)
+#     temp_G39 = edge_removing(unG39, temp_edge)
+#     temp_A39, temp_redL39, temp_redA39 = kron_reduction(n39, ngnr39, temp_G39)
+#     temp_model39 = PowerNetworkSolver(theta0, omega0, temp_redA39, ngnr39, D, M, K, OMEGA)
+#     temp_rates39 = temp_model39.Simulation(KK, check_times, sigma, thres, t, nn, cor_disturbances)
+#     temp_df39 = pd.DataFrame(
+#         {'Node': node_list, 'RoCoF': temp_rates39['vcheck_domega'], 'AFV': temp_rates39['vcheck_omega'],
+#          'AV': temp_rates39['vcheck_any']})
+#     rate_list39[j, :] = temp_df39.mean(axis=0)
+#     j += 1
