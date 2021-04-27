4e0de30b79bb0beaec12e4d5315c1510cb2e2d6b
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-04-27 19:19:37 +0200

    add the stop criterion

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -5,7 +5,6 @@ from disturbancesGnr import normaldisturbances
 from violationChecking import globalcheck
 import seaborn as sns
 
-
 sns.set_theme()
 # importing the function for multi-edge removal
 from powerNetwork import multi_edge_removing
@@ -18,13 +17,18 @@ def greedy_algorithm(graph, n, ngnr, int_theta, int_omega, D, M, K, OMEGA, KK, c
                      dtb_gnr, max_itr, type_rate):
     new_graph = deepcopy(graph)
     k = 0
+    tol = 1e-5
     node_list = np.arange(ngnr) + 1
-    while k <= max_itr:
+    new_rate = 1
+    # prev_rate = 1
+    continuation = True
+    while k <= max_itr and continuation:
         new_edge_list = list(new_graph.edges)
         new_num_edges = new_graph.number_of_edges()
-        rate_list = np.zeros((new_num_edges,4))
+        rate_list = np.zeros((new_num_edges, 4))
         new_edge = np.array(new_edge_list[0])  # initialize the edge
-        new_rate = 1  # initialize the rate
+        # new_rate = 1  # initialize the rate
+        prev_rate = new_rate
         j = 0
         for i in new_edge_list:
             temp_edge = np.array(i)
@@ -38,14 +42,19 @@ def greedy_algorithm(graph, n, ngnr, int_theta, int_omega, D, M, K, OMEGA, KK, c
             rate_list[j, :] = temp_df39.mean(axis=0)
             if rate_list[j, type_rate] < new_rate:
                 new_edge = temp_edge
-                prev_rate = new_rate
+                # prev_rate = new_rate
                 new_rate = rate_list[j, type_rate]
             j += 1
+        # prev_rate = new_rate
+        if np.abs(new_rate - prev_rate) <= tol:
+            continuation = False  # end the outer loop
+
         prev_graph = deepcopy(new_graph)
-        print(new_edge) #print the edge to be removed
+        # print(new_edge)  # print the edge to be removed
         new_graph = edge_removing(prev_graph, new_edge)
         k += 1
-        print(str(new_graph.number_of_edges())+"  " + str(k))
+        print(str(new_graph.number_of_edges()) + " : " + str(k) + ", and the edge is " +
+              str(new_edge))
     return new_graph
 
 # show edge list
